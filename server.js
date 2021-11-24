36b64ec3ec3fb2c120ecaaa8c2b21d42089e309a
kaveh <ka7eh@pm.me>
2021-11-23 19:46:35 -0600

    Restructure for Github Pages

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 const http = require("http");
-const path = require("path");
 
 http.createServer(function(req, res) {
-    const url = req.url === "/" ? "/index.html" : req.url;
-    const dir = url.startsWith("/lib/") ? path.join(__dirname, "../") : "./examples";
-    fs.readFile(`${dir}${url}`, function(err, data) {
+    const url = req.url === "/" ? "/example.html" : req.url;
+    fs.readFile(`${__dirname}${url}`, function(err, data) {
         if (err) {
             res.writeHead(404);
             res.end(JSON.stringify(err));
